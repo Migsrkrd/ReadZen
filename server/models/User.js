49993c31840f7d8a4ee45e0478afb762315b58a7
DistantDig <jameswaller711@gmail.com>
2023-12-08 12:55:40 -0800

    added User model

---
+++ server/models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String, 
+        required: true,
+        unique: true,
+        trim: TextTrackCue,
+        email: {
+            String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must be a valid email address!']
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 5
+        },
+        readMes: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'ReadMe'
+            }
+        ]
+        // Add pinned ReadMes later
+    }
+});
+
+// Hashes password
+userSchema.pre('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+    next();
+});
+
+// Checks to see if the given password is valid
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', userSchema);
+
+module.exports = User;
