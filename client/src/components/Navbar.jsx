1929d732c661c6497e88f78066f5a18a197ea9fe
James Hunter <jamessahunter@gmail.com>
2023-12-08 14:04:09 -0800

    add modal to home page

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,8 +6,26 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 
+
+const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 const Header = () => {
     const [showModal, setShowModal] = useState(false);
+
+    const [open, setOpen] = useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     const logout = (event) => {
         event.preventDefault();
         Auth.logout();
@@ -41,13 +59,30 @@ const Header = () => {
         </div>
             </div>
 
-        <Modal
+            <div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
+        {/* <Modal
         size='lg'
         show={showModal}
         onHide={() => setShowModal(false)}
         aria-labelledby='signup-modal'>
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey='login'>
+        <Container defaultActiveKey='login'>
           <Modal.Header closeButton>
             <Modal.Title id='signup-modal'>
               <Nav variant='pills'>
@@ -61,17 +96,17 @@ const Header = () => {
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
+            <Content>
+              <Pane eventKey='login'>
                 <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
+              </Pane>
+              <Pane eventKey='signup'>
                 <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
+              </Pane>
+            </Content>
           </Modal.Body>
-        </Tab.Container>
-      </Modal>
+        </Container>
+      </Modal> */}
       </header>
     )
 }
