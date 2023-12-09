76f1f91266a03a77427374034932501c543c16d9
DistantDig <jameswaller711@gmail.com>
2023-12-08 16:17:08 -0800

    added generate page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 import Home from './pages/Home.jsx'
 import Profile from './pages/Profile.jsx'
 import About from './pages/About.jsx'
+import Generate from './pages/Generate.jsx'
 //creates the routes for the site
 const router = createBrowserRouter([
   {
@@ -27,6 +28,10 @@ const router = createBrowserRouter([
         {
             path: '/about',
             element: <About />
+        },
+        {
+            path: '/generate',
+            element: <Generate />
         }
 
     ]
