9370806270e22dad418704676d7479b523454300
David Wright <145091475+d-a-v-i-d-w-r-i-g-h-t@users.noreply.github.com>
2023-12-08 17:05:19 -0800

    Update resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,6 +3,7 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
+
         // User's profile page
         me: async (parent, args, context) => {
             if (context.user) {
@@ -10,28 +11,33 @@ const resolvers = {
             }
             throw AuthenticationError;
         },
+
         // Returns all users. Only for dev
         users: async () => {
             return User.find().populate('readme');
         },
+
         // Another user's profile page
         user: async (parent, { userId }) => {
             return User.findOne({ _id: userId }).populate('readme');
         },
+
         // Returns all readmes of a given user
         readmes: async (parent, { username }) => {
-            const user = User.findOne({ username: username });
+            const user = await User.findOne({ username });
             const readmes = user.ReadMes;
 
             return readmes;
         },
+
         // Returns a single readme by id
-        readme: async (parent, { readmeId }) => {
-            return ReadMe.findOne({ _id: readmeId });
+        readme: async (parent, { readMeId }) => {
+            return ReadMe.findOne({ _id: readMeId }).populate('author');
         }
     },
     
     Mutation: {
+
         // Logs in a user and assigns a token
         login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
@@ -47,6 +53,7 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+
         // Creates a new user and assigns a token
         addUser: async (parent, args) => {
             const user = await User.create(args);
@@ -54,6 +61,7 @@ const resolvers = {
 
             return { token, user };
         },
+
         // Creates a new readme and adds it to user's readmes
         addReadMe: async (parent, args) => {
             const readme = await ReadMe.create({ ...args });
@@ -64,27 +72,31 @@ const resolvers = {
             );
             return readme;
         },
+
         // Edits a readme and updates a user's readmes
         updateReadMe: async (parent, args) => {
+            const { readMeId, ...updateArgs } = args;
             const readme = await ReadMe.findOneAndUpdate(
-                { _id: readmeId },
-                { ...args },
-                { new: true}
-                );
+                { _id: readMeId },
+                { ...updateArgs },
+                { new: true }
+            );
+
             await User.findOneAndUpdate(
-                { _id: readmeId },
-                { $pull: { ReadMes: { _id: readmeId }}},
-                { $addToSet: { ReadMes: readme }}
+                { _id: readMeId },
+                { $pull: { ReadMes: { _id: readMeId } } },
+                { $addToSet: { ReadMes: readme } }
             );
             return readme;
         },
+
         // Deletes a readme and removes it from a user's readmes
-        deleteReadMe: async (parent, { readmeId }) => {
-            const readme = await ReadMe.findOneAndDelete({ _id: readmeId });
+        deleteReadMe: async (parent, { readMeId }) => {
+            const readme = await ReadMe.findOneAndDelete({ _id: readMeId });
             await User.findOneAndUpdate(
-                { _id: readmeId },
-                { $pull: { ReadMes: { _id: readmeId }}},
-                { new: true}
+                { _id: readMeId },
+                { $pull: { ReadMes: { _id: readMeId } } },
+                { new: true }
             );
             return readme;
         }
