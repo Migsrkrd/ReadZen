7a9cf1495dab07cb0815be8d128015083aa0f230
DistantDig <jameswaller711@gmail.com>
2023-12-08 16:38:45 -0800

    fixed class and mutation naming

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,24 +1,24 @@
 const Footer = () => {
 
     return (
-<footer class="site-footer">
-  <div class="container">
-    <div class="footer-content">
-      <div class="footer-logo">
+<footer className="site-footer">
+  <div className="container">
+    <div className="footer-content">
+      <div className="footer-logo">
         <img src="logo.png" alt="ReadZen Logo"/>
       </div>
-      <div class="footer-info">
+      <div className="footer-info">
         <p>&copy; 2023 ReadZen. All Rights Reserved.</p>
         <p>Designed and Developed with ❤️ by Your Team Name</p>
         <p>Contact us: <a href="mailto:info@readzen.com">info@readzen.com</a></p>
       </div>
-      <div class="footer-social">
+      <div className="footer-social">
         <p>Follow us:</p>
         <ul>
-          <li><a href="#" target="_blank"><i class="fa fa-twitter"></i></a></li>
-          <li><a href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
-          <li><a href="#" target="_blank"><i class="fa fa-instagram"></i></a></li>
-          <li><a href="#" target="_blank"><i class="fa fa-github"></i></a></li>
+          <li><a href="#" target="_blank"><i className="fa fa-twitter"></i></a></li>
+          <li><a href="#" target="_blank"><i className="fa fa-facebook"></i></a></li>
+          <li><a href="#" target="_blank"><i className="fa fa-instagram"></i></a></li>
+          <li><a href="#" target="_blank"><i className="fa fa-github"></i></a></li>
         </ul>
       </div>
     </div>
