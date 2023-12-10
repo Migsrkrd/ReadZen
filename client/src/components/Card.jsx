b3924b0e5c35050e06e8412feaabdbb59ee4683f
James Hunter <jamessahunter@gmail.com>
2023-12-10 13:46:52 -0800

    add home page to display all readmes on cards

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,10 +4,9 @@ import { useState } from "react";
 import DisplayReadMe from "./DisplayReadMe";
 import Avatar from "./Avatar"
 
-const Card = ({ title, description, github, deploy, username }) => {
-  console.log('test')
-  const letter = username.charAt(0).toUpperCase();
+const Card = (props) => {
 
+  console.log(props.ReadMes)
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => {
@@ -19,20 +18,23 @@ const Card = ({ title, description, github, deploy, username }) => {
   };
 
   return (
+    <div>
+    {props.ReadMes.map((readme) => (
     <div className="card"  onClick={openModal}>
+      {console.log(readme)}
       <div className="card-header">
-        <Link className="profile-link" to={`/profiles/${username}`}>
-          <h4><Avatar letter={letter}/>@{username}</h4>
+        <Link className="profile-link" to={`/profiles/${readme.author}`}>
+          <h4><Avatar letter={readme.author.charAt(0).toUpperCase()}/>@{readme.author}</h4>
         </Link>
-        <h3>{title}</h3>
+        <h3>{readme.title}</h3>
       </div>
       <div className="card-body">
-        <p>{description}</p>
+        <p>{readme.description}</p>
         <div className="card-links">
-          <a href={github} target="_blank" rel="noopener noreferrer">
+          <a href={readme.reoLink} target="_blank" rel="noopener noreferrer">
             <i className="fa fa-github"></i>
           </a>
-          <a href={deploy} target="_blank" rel="noopener noreferrer">
+          <a href={readme.deployedLink} target="_blank" rel="noopener noreferrer">
             <i className="fa fa-link"></i>
           </a>
         </div>
@@ -40,12 +42,14 @@ const Card = ({ title, description, github, deploy, username }) => {
       {isModalOpen && (
         <DisplayReadMe
         onClose={closeModal}
-        username={username}
-        title={title}
-        description={description}
+        username={readme.username}
+        title={readme.title}
+        description={readme.description}
         />
       )}
     </div>
+  ))}
+  </div>
   );
 };
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -14,11 +14,15 @@ const Home = () => {
         <div className="divy">
           {loading ? 
           <h2>loading</h2> : 
-          ReadMes.map((readme)=> {
-            <Card title={readme.title} description={readme.description} github={readme.repoLink} deploy={readme.deployedLink} username={readme.username} />
-          })}
+          <Card ReadMes={ReadMes} /> }
+          {/* // ReadMes.map((readme)=> { */}
+          {/* //   <div>
+          //   <Card title={readme.title} description={readme.description} github={readme.repoLink} deploy={readme.deployedLink} username={readme.username} />
+          //   <Card title={"testing Again"} description={"whatsup"} github={"#"} deploy={"#"} username={"Mikey"}/>
+          //   </div>
+          // })} */}
         {/* <Card title={"testing"} description={"greanioup nvieoapnrg aerinug[naeoinvf ao[ienrgiua[ eonvfioe[aoienrg [aeaoiunfba [oiuerng "} github={"#"} deploy={"#"} username={"Mikey"}/>
-        <Card title={"testing Again"} description={"whatsup"} github={"#"} deploy={"#"} username={"Mikey"}/>
+        
         <Card title={"testing a third time"} description={"Im getting tired but i dont wanna stop"} github={"#"} deploy={"#"} username={"Lenny"}/> */}
         </div>
       </main>
