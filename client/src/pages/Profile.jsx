5bcfb797c11c14d5fb3c78242f57e4402aa5e74c
Mike <mreickcastillero@gmail.com>
2023-12-11 21:30:51 -0800

    styles profile cards, created header to be fixed, adjusted layout of main pages, and added more parameters to profile card

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -6,29 +6,33 @@ import { useQuery } from "@apollo/client";
 import { GET_READMES } from "../utils/queries";
 
 const Profile = () => {
-
-    const { loading, data } = useQuery(GET_READMES, {
-        variables: {username: (useLocation().pathname === '/me' ? `${Auth.getProfile().data.username}` : useParams().username)  },
-    });
-    const ReadMes = data?.readmes || [];
-    return (
-        <>
-        <h1>profile goes here</h1>
-        <div>
-            {useLocation().pathname === '/me'|| useParams().username===`${Auth.getProfile().data.username}` ? (
-        <div>
-       
-        <Link to="/generate">
-        Generate
-        </Link> 
-        <ProfileCard ReadMes={ReadMes}/>
-        </div>
-        )
-        
-        :  <Card ReadMes={ReadMes}/>}
-        </div>
-        </>
-    )
-}
+  const { loading, data } = useQuery(GET_READMES, {
+    variables: {
+      username:
+        useLocation().pathname === "/me"
+          ? `${Auth.getProfile().data.username}`
+          : useParams().username,
+    },
+  });
+  const ReadMes = data?.readmes || [];
+  return (
+    <>
+      <div>
+        {useLocation().pathname === "/me" ||
+        useParams().username === `${Auth.getProfile().data.username}` ? (
+          <div>
+            <div className="dashboard">
+              <h2>Dashboard</h2>
+              <Link className="generate" to="/generate">Generate</Link>
+            </div>
+            <ProfileCard ReadMes={ReadMes} />
+          </div>
+        ) : (
+          <Card ReadMes={ReadMes} />
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Profile;
